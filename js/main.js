c38b2a00e9adbe0b9d413aae12807c55d06fe007
Gabriel Barrientos <gabrielbarrientos729@gmail.com>
2021-09-03 11:56:49 -0300

    dom

--- js/main.js
+++ js/main.js
@@ -33,16 +33,52 @@ console.log(camisetasDeFutbol);
 
 //Primera entrega del proyecto final
 
-const camisetasQueValenMil= camisetasDeFutbol.filter(camisetaDeFutbol => camisetaDeFutbol.precio===1000);
+const camisetasQueValenMil = camisetasDeFutbol.filter(camisetaDeFutbol => camisetaDeFutbol.precio === 1000);
 console.log(camisetasQueValenMil);
 
-const aumentoDePrecio= camisetasDeFutbol.map(camisetaDeFutbol => camisetaDeFutbol.precio + 500);
+const aumentoDePrecio = camisetasDeFutbol.map(camisetaDeFutbol => camisetaDeFutbol.precio + 500);
 console.log(aumentoDePrecio);
 
-const camisetasDestacadas= camisetasDeFutbol.filter(camisetaDeFutbol => camisetaDeFutbol.esDestacada == true);
+const camisetasDestacadas = camisetasDeFutbol.filter(camisetaDeFutbol => camisetaDeFutbol.esDestacada == true);
 console.log(camisetasDestacadas);
 
-const barraDeBusqueda= camisetasDeFutbol.filter(camisetaDeFutbol => camisetaDeFutbol.nombre.includes("P"));
+const barraDeBusqueda = camisetasDeFutbol.filter(camisetaDeFutbol => camisetaDeFutbol.nombre.includes("P"));
 console.log(barraDeBusqueda);
 
 //
+
+/*
+let edadDeUsuario=parseInt(prompt("Ingresa tu edad"));
+let guardandoDato= localStorage.setItem("edad",edadDeUsuario);
+let obtenerDato=localStorage.getItem("edad");
+console.log(typeof obtenerDato);
+*/
+
+// Local storage y JSON
+let objeto1 = {
+    id: 1,
+    producto: "Pan"
+};
+let guardarElDato = localStorage.setItem("objeto", objeto1);
+let pasandoObjetoAString = JSON.stringify(objeto1);
+console.log(pasandoObjetoAString);
+let pasandoStringAObjeto = JSON.parse(pasandoObjetoAString);
+console.log(pasandoStringAObjeto);
+
+
+// Tarea de DOM
+    let articulos= document.getElementsByClassName("comprarArticulos");
+    let span = document.getElementById("cantidadDeCosasEnCarrito");
+    let carrito = span.innerHTML;
+    let carritoVacio = 0;
+
+for(let i=0; i<articulos.length; i++){
+    articulos[i].addEventListener("click",sumarUnoAlCarrito)
+}
+
+function sumarUnoAlCarrito() {
+    carritoVacio += 1;
+    carrito = `${carritoVacio}`;
+    span.innerHTML = carrito;
+
+}
